cff8152a996b300f70fda0e3ad9f79315d34fca0
tjdalsckd <53217819+tjdalsckd@users.noreply.github.com>
2021-08-24 16:11:38 +0900

    Update TCPClient2.cpp

--- TCPClient2.cpp
+++ TCPClient2.cpp
@@ -43,7 +43,7 @@ enum {
 	SIZE_HEADER = 52,
 	SIZE_COMMAND = 4,
 	SIZE_HEADER_COMMAND = 56,
-	SIZE_DATA_MAX = 200,
+	SIZE_DATA_MAX = 33,
 	SIZE_DATA_ASCII_MAX = 32
 };
 
@@ -51,7 +51,6 @@ enum {
 union Data
 {
 	unsigned char byte[SIZE_DATA_MAX];
-	float value[9];
 };
 
 int main(int argc, char **argv)
@@ -59,7 +58,7 @@ int main(int argc, char **argv)
 	StreamSocket ss;
 
 	Data data;
-	unsigned char writeBuff[1024];
+	unsigned char writeBuff[33];
 	
 	try
 	{
@@ -71,6 +70,15 @@ int main(int argc, char **argv)
 		{
 			cout << "Connecting to server..." << endl;
 		}
+		writeBuff[0] =  0x00;
+		    for(int i=0 ; i<16 ; i++){
+			writeBuff[i*2+1] = 0x80;
+			writeBuff[i*2+2] = 0x00;
+		    }
+		memcpy(writeBuff, data.byte, SIZE_DATA_MAX);    
+	        ss.sendBytes(writeBuff, 1024);
+//	        ss.sendBytes(writeBuff, 33);
+		cout << "Success" << endl;
 		ss.close();
 	}
 	catch (Poco::Exception& exc)
